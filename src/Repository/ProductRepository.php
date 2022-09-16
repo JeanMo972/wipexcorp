db4130fd7a7690f6d2b83bb3a3e6fc1e27bc2e4a
JeanMo972 <106687045+JeanMo972@users.noreply.github.com>
2022-09-16 12:53:03 -0400

    update projet wipexcorp 16-09-22

---
+++ src/Repository/ProductRepository.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Repository;
+
+use App\Classe\Search;
+use App\Entity\Product;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+
+/**
+ * @extends ServiceEntityRepository<Product>
+ *
+ * @method Product|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Product|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Product[]    findAll()
+ * @method Product[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ProductRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Product::class);
+    }
+
+    public function add(Product $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Product $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+/**
+     * Requête qui me permet de récupérer les produits en fonction de la recherche de l'utilisateur
+     * @return Product[]
+     */
+    public function findWithSearch(Search $search) {
+
+        $query = $this
+            // Je créer la requete à l'aide de la table product
+            ->createQueryBuilder('p')
+            // Je sélecte les tables catégory et product
+            ->select('c', 'p')
+              // Je join la catégory dans la table product et category dans la table category
+            ->join('p.category', 'c');
+
+            // Si l'uilisateur renseigne un catégorie à rechercher
+        if (!empty($search->categories)) {
+            $query = $query
+                ->andWhere('c.id IN (:categories)')
+                ->setParameter('categories', $search->categories);
+        }
+            //si l'utilisateur renseigne un texte à rechercher
+        if (!empty($search->string)) {
+            $query = $query
+                ->andWhere('p.name LIKE :string')
+                ->setParameter('string', "%$search->string%");
+        }
+
+        //crée la requête et la retourne 
+        return $query->getQuery()->getResult();
+    }
+
+       
+
+        
+        }
+
+       
+//    /**
+//     * @return Product[] Returns an array of Product objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('p')
+//            ->andWhere('p.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('p.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Product
+//    {
+//        return $this->createQueryBuilder('p')
+//            ->andWhere('p.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+
